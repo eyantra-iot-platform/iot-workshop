194179677cba11602317d30b1efa562d59066aab
Omkar Manjrekar <manjrekarom@gmail.com>
2018-07-05 19:04:32 +0530

    Simple PubSub code tested

--- iot-platform/connecting-devices-to-platform/python/simple-pubsub.py
+++ iot-platform/connecting-devices-to-platform/python/simple-pubsub.py
@@ -2,20 +2,23 @@ import json
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
 # Thing information
-ThingID = 22
+ThingID = "25"
+ClientID = "myClientID" 
+IoTEndpoint = "akbmorjah98q5.iot.ap-southeast-1.amazonaws.com"
+CERTIFICATE_PATH = "./certificates"
 
 # Change to your topics here
 UPDATE_TOPIC = "$aws/things/thing" + ThingID + "/shadow/update"
 DELTA_TOPIC = "$aws/things/thing" + ThingID + "/shadow/update/delta"
-CERTIFICATE_PATH = "./certificates"
+
 ROOT_CA = CERTIFICATE_PATH + "/rootCA.pem"
 PRIVATE_KEY = CERTIFICATE_PATH + "/private.key.pem"
 CERTIFICATE_CRT = CERTIFICATE_PATH + "/certificate.crt.pem"
 
 
 # Configuration for AWS IoT
-myMQTTClient = AWSIoTMQTTClient("myClientID")
-myMQTTClient.configureEndpoint("akbmorjah98q5.iot.ap-southeast-1.amazonaws.com", 8883)
+myMQTTClient = AWSIoTMQTTClient(ClientID)
+myMQTTClient.configureEndpoint(IoTEndpoint, 8883)
 myMQTTClient.configureCredentials(ROOT_CA, PRIVATE_KEY, CERTIFICATE_CRT)
 # myMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
 # myMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
@@ -26,19 +29,20 @@ myMQTTClient.enableMetricsCollection()
 
 # Connect to MQTT broker
 connected = myMQTTClient.connect()
-print("Connected:-", connected)
+print("Connected: ", connected)
 
 
 # Dictionaries and messages for publishing
-publish_dict = {'state': {'reported': {'device24.65': 29, 'device24.66': 12}}}
+publish_dict = {'state': {'reported': {'device26.69': 29, 'device26.70': 12, 'device27.71': 'Hello World!'}}}
 publish_message = json.dumps(publish_dict)
-print("Message to publish:-", publish_message)
+print("Message to publish: ", publish_message)
 
+def print_message(client, usrData, msg):
+    print(msg.payload)
+
+# Listen for state change
+myMQTTClient.subscribe(DELTA_TOPIC, 1, print_message)
 
 # Update device shadow
 published = myMQTTClient.publish(UPDATE_TOPIC, publish_message, 0)
-print("Published:-", published)
-
-
-# Listen for state change
-myMQTTClient.subscribe(DELTA_TOPIC, 1, lambda client, usrData, msg: print(msg.payload))
+print("Published: ", published)
