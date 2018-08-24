d60ed7415fff30a803e272ecd0558a9ec5df9677
Omkar Manjrekar <manjrekarom@gmail.com>
2018-08-24 14:37:30 +0530

    Added subscribe handle

--- iot-platform/connecting-devices-to-platform/python/codes/simple-pubsub.py
+++ iot-platform/connecting-devices-to-platform/python/codes/simple-pubsub.py
@@ -1,10 +1,11 @@
 import json
+import time
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
 
 # Thing information
-THING_ID = "25"
-CLIENT_ID = "myClientID" 
-ENDPOINT = "akbmorjah98q5.iot.ap-southeast-1.amazonaws.com"
+THING_ID = "58"
+CLIENT_ID = "myClientID58" 
+ENDPOINT = "a221a6r4ojicsi.iot.ap-southeast-1.amazonaws.com"
 CERTIFICATE_PATH = "./certificates"
 
 # Change to your topics here
@@ -30,19 +31,32 @@ myMQTTClient.enableMetricsCollection()
 # Connect to MQTT broker
 connected = myMQTTClient.connect()
 print("Connected: ", connected)
+time.sleep(0.5)
 
-
-# Dictionaries and messages for publishing
-publish_dict = {'state': {'reported': {'device26.69': 29, 'device26.70': 12.0, 'device27.71': 'Hello World!'}}}
-publish_message = json.dumps(publish_dict)
-print("Message to publish: ", publish_message)
 
 def print_message(client, usrData, msg):
     print(msg.payload)
 
+
 # Listen for state change
 myMQTTClient.subscribe(DELTA_TOPIC, 1, print_message)
+time.sleep(0.5)
 
-# Update device shadow
-published = myMQTTClient.publish(UPDATE_TOPIC, publish_message, 0)
-print("Published: ", published)
+TEMP = 25
+HUMD = 30
+
+while True:
+    # Dictionaries and messages for publishing
+    publish_dict = {'state': {'reported': {'device119.298': TEMP, 'device119.299': HUMD, 'device120.300': 'Hello World!'}}}
+    publish_message = json.dumps(publish_dict)
+    print("Message to publish: ", publish_message)
+   
+    # Update device shadow
+    published = myMQTTClient.publish(UPDATE_TOPIC, publish_message, 0)
+    
+    # change values
+    TEMP = (TEMP + 1) % 16 + 25 # temperature between 25 to 40
+    HUMD = (humd + 1) % 31 + 40 # humidity between 31 to 40 
+
+    print("Published: ", published)
+    time.sleep(5)
